2856467d6a8cfce555f955968ae68a9ff853c0aa
capellanproductions <capellanproductions.rd1@gmail.com>
2025-03-31 13:34:10 -0300

    Add files via upload

--- tienda/productosa/p1/components/ui/src/textinput - copia.js
+++ tienda/productosa/p1/components/ui/src/textinput - copia.js
@@ -6,18 +6,18 @@
             'disabled': false,
             'text': "",
             'position': 'absolute',
-            'rows': 5,
-            'cols': 30,
-            'fontSize': '25px' // Tamaño de la fuente
+            'rows': 5, // Número de líneas
+            'cols': 30 // Tamaño de las líneas
         },
-        _props: ["left", "top", "width", "height", "position", "transform-origin", "transform", "rows", "cols", "fontSize"],
+        _props: ["left", "top", "width", "height", "position", "transform-origin", "transform", "rows", "cols"],
         _attrs: ["id", "disabled", "class", "rows", "cols"],
         getCreateOptions: function() {
             return $.extend(this.options, { 'id': "textinput" + _widgetID++ });
         },
         getCreateString: function() {
+            // Se eliminan los bordes y el fondo del textarea
             return "<textarea id='" + this.options.id + "' rows='" + this.options.rows + "' cols='" + this.options.cols + 
-                   "' style='resize: none; border: none; outline: none; background: transparent; font-size: " + this.options.fontSize + ";'></textarea>";
+                   "' style='resize: none; border: none; outline: none; background: transparent;'></textarea>";
         },
         getProperties: function() {
             return this._props;
