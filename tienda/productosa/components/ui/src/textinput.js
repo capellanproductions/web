262fd8025d00c664ef5d2108d1c3ceb3a3106c6b
capellanproductions <capellanproductions.rd1@gmail.com>
2025-03-31 21:04:21 -0300

    Add files via upload

---
+++ tienda/productosa/components/ui/src/textinput.js
@@ -0,0 +1,30 @@
+(function($) {    
+
+    $.anwidget("an.TextInput", {
+        options: {
+            'visible': true,
+            'disabled': false,
+            'text': "",
+            'position': 'absolute',
+            'rows': 5,
+            'cols': 30,
+            'fontSize': '25px' // Tamaño de la fuente
+        },
+        _props: ["left", "top", "width", "height", "position", "transform-origin", "transform", "rows", "cols", "fontSize"],
+        _attrs: ["id", "disabled", "class", "rows", "cols"],
+        getCreateOptions: function() {
+            return $.extend(this.options, { 'id': "textinput" + _widgetID++ });
+        },
+        getCreateString: function() {
+            return "<textarea id='" + this.options.id + "' rows='" + this.options.rows + "' cols='" + this.options.cols + 
+                   "' style='resize: none; border: none; outline: none; background: transparent; font-size: " + this.options.fontSize + ";'></textarea>";
+        },
+        getProperties: function() {
+            return this._props;
+        },
+        getAttributes: function() {
+            return this._attrs;
+        }
+    });
+
+})(jQuery);
